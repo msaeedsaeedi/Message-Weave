3dc20e12cd343edb43ccdc384b2767bd42fae9fb
Mohammad Saeed <msaeedsaeedi@outlook.com>
2024-04-13 03:03:19 +0500

    Chatroom Basic Design

--- src/app/page/chatroom/chatroom.component.ts
+++ src/app/page/chatroom/chatroom.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface Message {
+  message: string,
+  sentby: string
+}
 
 @Component({
   selector: 'app-chatroom',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './chatroom.component.html',
   styleUrl: './chatroom.component.css'
 })
-export class ChatroomComponent {
 
+export class ChatroomComponent implements OnInit {
+  messages: Message[] = [
+    {
+      message: "Hi",
+      sentby: 'Sana Tariq'
+    },
+    {
+      message: "Hello",
+      sentby: 'Mohammad Saeed'
+    }
+  ];
+
+  message: string = '';
+
+  ngOnInit(): void {
+
+  }
+
+  send(message: string): void {
+    console.log(message);
+
+  }
 }
