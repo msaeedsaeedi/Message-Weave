15582142318b4ee093c5d3aac76f27608c45b8d8
Mohammad Saeed <msaeedsaeedi@outlook.com>
2024-04-13 00:11:38 +0500

    Routing
    - login
    - register
    - chatroom

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./page/chatroom/chatroom.component').then(mod => mod.ChatroomComponent)
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./page/login/login.component').then(mod => mod.LoginComponent)
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./page/register/register.component').then(mod => mod.RegisterComponent)
+    }
+];
