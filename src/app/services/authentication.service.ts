07413f9857d0eccfe821c6ce4b7e79cd180b546a
Mohammad Saeed <msaeedsaeedi@outlook.com>
2024-04-13 01:10:01 +0500

    Updated
    Implemented Functions :-
    - Login
    - Logout
    - IsloggedIn

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { account } from '@lib/appwrite';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  private loggedInUser: any;
+
+  public async Login(email: string, password: string): Promise<boolean> {
+    try {
+      await account.createEmailSession(email, password);
+      this.loggedInUser = await account.get();
+      return true;
+    }
+    catch {
+      return false;
+    }
+  }
+
+  public async Logout(): Promise<boolean> {
+    try {
+      await account.deleteSession('current');
+      return true;
+    }
+    catch {
+      return false;
+    }
+  }
+
+  public IsLoggedIn(): boolean {
+    if (this.loggedInUser)
+      return true;
+    return false;
+  }
+
 }
